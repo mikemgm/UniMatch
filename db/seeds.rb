6c486f8a53d65d362284dbeb738a1355ba988be4
Michael G. McCallum <mikemccallum@michaels-mbp.ait-sydney.net.au>
2018-10-23 16:42:56 +1100

    Generated seeds for home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+50.times do |i| 
+    School.create( 
+        image: Faker::LoremFlickr.image("50x60"),
+        name: Faker::University.name, 
+        location: Faker::Address.full_address, 
+        website: Faker::Internet.url,
+        rank: rand(100),
+        cost: rand(20000),
+        student_num: rand(20000),
+        content: Faker::Lorem.paragraph,
+        acceptance_rate: rand(100).to_f
+
+    )
+end 
