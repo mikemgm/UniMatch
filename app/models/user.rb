7ffdfbed677962d55a01b28b48ae859e283f59d1
Michael G. McCallum <mikemccallum@michaels-mbp.ait-sydney.net.au>
2018-10-24 17:11:49 +1100

    fixed user login

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
+
+# make model named favories <-- this will be the joint table 
+# put has_many relationship for users and schools 
